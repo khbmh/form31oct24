de41e5caa546dd159aa06ed13f4ce0b4b704f9db
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2024-10-31 16:08:26 +0600

    form body done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SimpleForm from './components/simpleform/SimpleForm';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center my-3'>Hello</h1>
+      <SimpleForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/simpleform/SimpleForm.jsx
@@ -0,0 +1,51 @@
+function SimpleForm() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submit');
+    console.log(e.target.email.value);
+    console.log(e.target.password.value);
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <form className="card-body" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SimpleForm;
